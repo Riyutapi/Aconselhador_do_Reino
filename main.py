5e0eac4f57e947cc6ac6a5e9787076658608b0b9
Ricardo Yudi Takahashi Pimentel <109813670+Riyutapi@users.noreply.github.com>
2023-06-23 19:13:43 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import pygame
-import sys
-import os
+import pygame, sys, os
 from tela_inicial import tela_inicial
 from corrida_balao import corrida_balao
 from paintball import paintball
@@ -12,79 +10,97 @@ from cartas import cartas
 
 # Inicialização do pygame
 pygame.init()
+
 # Definindo as dimensões da tela
 largura_tela = 800
 altura_tela = 600
+
 tela = pygame.display.set_mode((largura_tela, altura_tela))
+
 # Icone e nome do game
 pygame.display.set_caption("Aconselhador do Reino")
 pygame.display.set_icon(pygame.image.load('icone.jpeg'))
+
 # Variável para controlar o estado atual do jogo
-estado_jogo = "tela_inicial"
-arquivo = os.path.join("save", "save.txt")
+estado_jogo = "tela_inicial" # Quando abrir o jogo sempre começará na tela inicial
+
+
+def verificar():
+    """
+    Função para verificar o estado atual do jogo com base no arquivo save.
+
+    A função verifica a existência do arquivo save e extrai o conteúdo para determinar o estado do jogo em que o jogador parou.
+    O estado do jogo é determinado pelo valor armazenado no arquivo save na linha correspondente ao avanço do jogador.
+
+    Returns:
+        estado_jogo (str): O estado atual do jogo. Pode ser do minigame1 até o minigame7.
+
+    """
+    arquivo = os.path.join("save", "save.txt")
+
+    if os.path.exists(arquivo):  # Verificar se o arquivo save existe
+        with open(arquivo, 'r') as file:
+            conteudo = file.read()
+            linhas = conteudo.split("\n")
+            for linha in linhas:
+                if "avancar = " in linha: # Verificar em qual minigame o jogador parou
+                    valor_avancar = linha.split("=")[-1].strip()
+                    padrao = int(valor_avancar)
+            if padrao == 1:
+                estado_jogo = "minigame1"
+            elif padrao == 2:
+                estado_jogo = "minigame2"
+            elif padrao == 3:
+                estado_jogo = "minigame3"
+            elif padrao == 4:
+                estado_jogo = "minigame4"
+            elif padrao == 5:
+                estado_jogo = "minigame5"
+            elif padrao == 6:
+                estado_jogo = "minigame6"
+            elif padrao == 7:
+                estado_jogo = "minigame7"
+
+    return estado_jogo
+
+
 # Loop principal do jogo
 while True:
+
     if estado_jogo == "tela_inicial":
-        # Chama a função da tela inicial
-        inicio = tela_inicial(tela)
-        if os.path.exists(arquivo):  # Verificar se o arquivo save existe
-            with open(arquivo, 'r') as file:
-                conteudo = file.read()
-                linhas = conteudo.split("\n")
-                for linha in linhas:
-                    if "avancar = " in linha: # Verificar em qual minigame o jogador parou
-                        valor_avancar = linha.split("=")[-1].strip()
-                        padrao = int(valor_avancar)
-                if padrao == 1:
-                    estado_jogo = "minigame1"
-                elif padrao == 2:
-                    estado_jogo = "minigame2"
-                elif padrao == 3:
-                    estado_jogo = "minigame3"
-                elif padrao == 4:
-                    estado_jogo = "minigame4"
-                elif padrao == 5:
-                    estado_jogo = "minigame5"
-                elif padrao == 6:
-                    estado_jogo = "minigame6"
-                elif padrao == 7:
-                    estado_jogo = "minigame7"
-                else:
-                    estado_jogo = "sair"
+        tela_inicial(tela) 
+        estado_jogo = verificar()
+
     elif estado_jogo == "minigame1":
-        # Chama a função do primeiro minigame
         corrida_balao(tela)
+        estado_jogo = verificar()
+
     elif estado_jogo == "minigame2":
-        # Chama a função do segundo minigame
-        paintball()
+        paintball(tela)
         pygame.quit()
         sys.exit()
+
     elif estado_jogo == "minigame3":
-        # Chama a função do terceiro minigame
         futebol_carroca()
         pygame.quit()
         sys.exit()
+
     elif estado_jogo == "minigame4":
-        # Chama a função do quarto minigame
         decisoes()
         pygame.quit()
         sys.exit()
+
     elif estado_jogo == "minigame5":
-        # Chama a função do quinto minigame
         debate()
         pygame.quit()
         sys.exit()
+
     elif estado_jogo == "minigame6":
-        # Chama a função do sexto minigame
         rpg()
         pygame.quit()
         sys.exit()
+
     elif estado_jogo == "minigame7":
-        # Chama a função do sétimo minigame
         cartas()
-        pygame.quit()
-        sys.exit()
-    elif estado_jogo == "sair":
-        # Fecha o jogo
         pygame.quit()
         sys.exit()
--- paintball.py
+++ paintball.py
@@ -1,4 +1,23 @@
-def paintball():
-    # Código da função paintball
-    print('\n\n\n\nDeu certo paintball\n\n\n\n')
-    
+import pygame, sys
+from comum import exibir_mensagem
+
+pygame.init
+
+def paintball(tela):
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:  # Fechar o jogo pela janela
+                pygame.quit()
+                sys.exit()
+        exibir_mensagem("Continua...", 255, 255, 255, 50, 0, 0, tela)
+        pygame.display.flip()
+        tempo_inicial = pygame.time.get_ticks()
+        while pygame.time.get_ticks() - tempo_inicial < 3000:  # Esperar mensagem
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:  # Fechar o jogo pela janela
+                    pygame.quit()
+                    sys.exit()
+                elif evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:  #Ignorar clicke do mouse
+                    pass
+        pygame.quit()
+        sys.exit()
